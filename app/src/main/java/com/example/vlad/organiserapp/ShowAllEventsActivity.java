18e55ae6405be0ba15c7e761378c170c0afa2888
Vladdd97 <vladbantus@mail.ru>
2018-09-30 22:55:48 +0300

    Added :    - ShowAllEventsActivity       - now every event has its own modify and delete button (need to add functionality to these buttons)

--- app/src/main/java/com/example/vlad/organiserapp/ShowAllEventsActivity.java
+++ app/src/main/java/com/example/vlad/organiserapp/ShowAllEventsActivity.java
@@ -1,9 +1,11 @@
 package com.example.vlad.organiserapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,6 +14,10 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class ShowAllEventsActivity extends AppCompatActivity {
+
+    public int textViewIncreaseIndex = 0;
+    public int modifyButtonIncreaseIndex = 1000;
+    public int deleteButtonIncreaseIndex = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +30,49 @@ public class ShowAllEventsActivity extends AppCompatActivity {
 
     public void showAllEvents(){
 
-        LinearLayout eventsLinearLayout = findViewById(R.id.eventsLinearLayout);
+        // get parent Layout
+        LinearLayout parentLinearLayout = findViewById(R.id.eventsLinearLayout);
         ArrayList<CustomEvent> eventList;
         eventList = CustomEventXml.getcustomEvents();
         for( int i = 0 ; i < eventList.size(); i++){
 
+            // create child LinearLayout for TextView
+            LinearLayout childTextViewLinearLayout = new LinearLayout(ShowAllEventsActivity.this);
+
+            // create new TextView
             TextView newTextView = new TextView(ShowAllEventsActivity.this);
-            newTextView.setId( eventList.get(i).getId() );
+            newTextView.setId(textViewIncreaseIndex + eventList.get(i).getId());
             newTextView.setText("\n" + "" +
                     "ID : " + eventList.get(i).getId() + "\n" +
                     "Title : " + eventList.get(i).getTitle() + "\n" +
                     "Description : " + eventList.get(i).getDescription() + "\n" +
                     "Date : " + eventList.get(i).getDate());
-            eventsLinearLayout.addView(newTextView);
+
+            // create child LinearLayout for Buttons
+            LinearLayout childButtonswLinearLayout = new LinearLayout(ShowAllEventsActivity.this);
+
+            // create modify button
+            Button modifyButton = new Button(ShowAllEventsActivity.this);
+            modifyButton.setId(modifyButtonIncreaseIndex + eventList.get(i).getId());
+            modifyButton.setText("Modify");
+            modifyButton.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_dark));
+
+            // create delete button
+            Button deleteButton = new Button(ShowAllEventsActivity.this);
+            deleteButton.setId(deleteButtonIncreaseIndex + eventList.get(i).getId());
+            deleteButton.setText("Delete");
+            deleteButton.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
+
+            // add TextView to child LinearLayout for TextView
+            childTextViewLinearLayout.addView(newTextView);
+
+            // add Buttons to child LinearLayout for Buttons
+            childButtonswLinearLayout.addView(modifyButton);
+            childButtonswLinearLayout.addView(deleteButton);
+
+            // add childLinearLayouts to parent LinearLayout
+            parentLinearLayout.addView(childTextViewLinearLayout);
+            parentLinearLayout.addView(childButtonswLinearLayout);
         }
 
     }
